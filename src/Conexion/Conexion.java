b975166ce80f0883a897223840e21bb1881b16dc
JOSE ORELLANA <JOSE ORELLANA@LAPTOP-5ALV790M>
2025-09-17 10:37:40 -0500

    Commit al Proyecto de Algoritmos y Estructura de Datos

---
+++ src/Conexion/Conexion.java
@@ -0,0 +1,34 @@
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+public class Conexion {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/AUN NO TENEMOS";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConexion() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Error al conectar: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Connection con = getConexion()) {
+            if (con != null) {
+                System.out.println("Conexión exitosa a la base de datos!");
+            } else {
+                System.out.println("No se pudo establecer la conexion.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al conectar o cerrar: " + e.getMessage());
+        }
+    }
+}
